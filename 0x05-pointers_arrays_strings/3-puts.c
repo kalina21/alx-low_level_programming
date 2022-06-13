d40dc67280d29a67590379b04894c9dc8775440b
Kalina21 <kidan4259@gmail.com>
2022-06-13 06:27:06 -0700

    p

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line to stdout
+ * @str: stores input value
+ */
+void _puts(char *str)
+{
+	int c = 0;
+
+
+
+	while (c >= 0)
+	{
+		if (str[c] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[c]);
+		c++;
+
+	}
+}
+
